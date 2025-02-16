a471d8162d911fd26b610a24e2b015a42367ce60
Muhammed <muhammedsosun06@gmail.com>
2025-02-16 18:30:20 +0300

    update

--- src/main/java/com/MuhammedSosun/_2_week/example.java
+++ src/main/java/com/MuhammedSosun/_2_week/example.java
@@ -11,14 +11,19 @@ public class example {
 
 
         for (int i = 1; i <= n; i++) {
-            if(i == 47)
+            if(i == 47) {
+                System.out.println("in your number there is 47 so we past 47");
                 continue;
-            sum = i + sum;
-            if (i>100)
+            }
+            if (i>100) {
+                System.out.println("your number is bigger than 100 so just we add to 100");
                 break;
+            }
+
+            sum = i + sum;
         }
 
-        System.out.println("toplam : " + sum);
+        System.out.println("sum : " + sum);
         if (sum % 2 == 0){
             System.out.println("Number is even number");
         }
