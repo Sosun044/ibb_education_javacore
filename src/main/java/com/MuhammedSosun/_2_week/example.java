42779d5d5f4a1b1cd19180e4e3ab619eea878502
Muhammed <muhammedsosun06@gmail.com>
2025-02-17 14:59:22 +0300

    update

--- src/main/java/com/MuhammedSosun/_2_week/example.java
+++ src/main/java/com/MuhammedSosun/_2_week/example.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class example {
     public static void main(String[] args) {
-        int n,sum= 0;
         Scanner scanner = new Scanner(System.in);
+
+        /*
+        int n,sum= 0;
+
         System.out.println("please enter a number but do not enter zero(0): ");
         n = scanner.nextInt();
 
@@ -30,6 +33,58 @@ public class example {
         else {
             System.out.println("Number is odd number");
         }
+
+         */
+        /* prime number
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        boolean temp = true;
+        for (int i = 2; i < n; i++) {
+
+            if (n % i == 0) {
+                temp = false;
+                break;
+            }
+        }
+        if (temp == true){
+            System.out.println("The number is  prime number");
+        }
+        else {
+            System.out.println("The number is not prime number");
+        }
+        */
+        /*
+        int n=scanner.nextInt();
+        int firstTerm = 0,secondTerm = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(firstTerm + ",");
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+
+         */
+        /*
+        int number ,digits = 0;
+        System.out.println("Enter a number: ");
+        number = scanner.nextInt();
+        while (number >=10) {
+            digits = digits + (number % 10);
+            number = number/10;
+        }
+        digits = digits + number;
+        System.out.println(digits + " sum of digits");
+
+         */
+
+        int number ,digits = 0;
+        System.out.println("Enter a number: ");
+        number = scanner.nextInt();
+
+
 
     }
 }
